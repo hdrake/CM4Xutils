5c8fde44abd4c60938c7934984ed87f106bc76de
Henri Drake <henrifdrake@gmail.com>
2025-11-11 15:44:09 -0500

    Reran example notebooks and bumped dataset versions
    Had to add an additional rechunking call for age for the tracer
    processing script to run without errors.

--- scripts/coarsen_sigma2_budgets.py
+++ scripts/coarsen_sigma2_budgets.py
@@ -18,6 +18,6 @@ for start_year in np.arange(interval_start, interval_start+interval_length, 5):
     filename = f"../data/coarsened/{model}_budgets_sigma2_{year_range}.zarr"
     ds = remap_budgets_to_sigma2_and_coarsen(model, start_year)
     ds = ds.chunk({"time":1, "time_bounds":1})
-    ds.attrs["version"] = "v0.4.0"
-    ds.attrs["version_notes"] = """Between v0.3.0 and v0.4.0, CM4Xutils has been upgraded to v0.7.0. The main change is that the 'd2' coarsening bug (see https://github.com/hdrake/CM4Xutils/issues/6) has been fixed. This was a bug in the online MOM6 diagnostic manager, which used incorrect masking of weights in the area-weighted averaging of diagnostics with the 'xh:mean yh:mean z_l:sum' cell methods. This affected may of the foundational variables for water mass budget analysis, including the thickness of coarsened cells 'thkcello' and all heat and salt budget tendencies. This bug only affects the CM4Xp125 diagnostics, because they were downsampled by a factor of 2x2 (i.e. 'd2') whereas CM4Xp25 was output at its full native resolution."""
+    ds.attrs["version"] = "v0.5.0"
+    ds.attrs["version_notes"] = """Between v0.4.0 and v0.5.0, CM4Xutils has been upgraded to v0.8.0. The main change is that the sigma2 levels were extended so that extremely dense water masses are now included in the target coordinate grid! This ensures global mass and energy conservation."""
     ds.to_zarr(filename, mode="w")
